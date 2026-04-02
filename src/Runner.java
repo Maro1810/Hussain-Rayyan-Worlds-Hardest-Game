92621ea64a797ca00a15e7a77863e83d1a924b71
1929171 <1929171@01-18100.pusd.dom>
2026-04-02 10:56:04 -0700

    made src folder and started basic set up for one Jframe

---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame menu = new JFrame("Main Menu");	
+		menu.setSize(1000, 1000);
+		menu.setVisible(true);
+		
+	}
+	
+
+}

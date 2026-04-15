e01e1f2d5f40c452b819291829738cd344dc8b37
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-04-15 11:58:51 -0700

    template class for obstacles (i.e. walls, balls)

---
+++ src/Obstacle.java
@@ -0,0 +1,40 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+public abstract class Obstacle {
+
+    protected boolean kills;
+
+    public Obstacle(boolean kills) {
+        this.kills = kills;
+    }
+
+    public abstract void paint(Graphics g);
+
+    public void move() {}
+
+    public abstract void collision(Player p);
+
+    protected abstract void init(double a, double b);
+
+    public void setVx(int vx) {}
+
+    public void setVy(int vy) {}
+
+    public void setX(int x) {}
+
+    public void setY(int y) {}
+
+    protected Image getImage(String path) {
+        Image tempImage = null;
+        try {
+            URL imageURL = Obstacle.class.getResource(path);
+            tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tempImage;
+    }
+}
